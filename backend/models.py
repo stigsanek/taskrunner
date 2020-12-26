a0b763c08cded3510e6b0dece6b906133e02685f
stigsanek <stigsanek@yandex.ru>
2020-12-26 14:08:19 +0400

    Создает модель и файл миграции

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Описание', max_length=255)
+    close_date = models.DateField('Дата завершения')
+    is_complete = models.BooleanField('Выполнено', default=False)
+    is_important = models.BooleanField('Важная', default=False)
+    owner = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
