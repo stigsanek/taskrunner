c4d9e9d02262fdbfcf8c42d809c086d4ae154963
stigsanek <stigsanek@yandex.ru>
2020-12-26 14:42:13 +0400

    Добавляет настройку отключения интерфейса API

--- taskrunner/settings.py
+++ taskrunner/settings.py
@@ -121,6 +121,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
